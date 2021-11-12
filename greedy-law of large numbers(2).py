bb068b8541fc40cc8e3234e02faace35bd939300
jinyeong-afk <xoeh7002@naver.com>
2021-11-13 00:56:33 +0900

    그리디 - 큰 수의 법칙(2)

--- greedy-law of large numbers(2).py
+++ greedy-law of large numbers(2).py
@@ -1,5 +1,3 @@
-#n,m,k
-
 n, m, k = map(int, input().split())
 num = list(input().split())
 sum = 0
@@ -21,3 +19,22 @@ while True:
     count += 1
 
 print(sum)
+
+##########################
+
+n, m ,k = map(int, input().split())
+num = list(map(int, input().split()))
+
+num.sort(reverse=True)
+result = 0
+first = num[0]
+second = num[1]
+
+count = int(m/(k+1)) * k
+count += m % (k+1)
+
+
+result = count * first
+result += (m-count) * second
+
+print(result)
